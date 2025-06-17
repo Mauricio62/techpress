31242dc3338a4bc865036b29e6c3b40ece13883f
Mauricio62 <ericksandovalv08@gmail.com>
2025-06-17 17:30:13 -0500

    Archivos iniciales y de configuración

---
+++ src/main/java/pe/com/techpress/ProjectTechPress/ProjectTechPressApplication.java
@@ -0,0 +1,13 @@
+package pe.com.techpress.ProjectTechPress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTechPressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTechPressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/techpress/ProjectTechPress/ProjectTechPressApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.techpress.ProjectTechPress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTechPressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
