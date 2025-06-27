c9e9146c3fd9d33b91805359e97f8f1d22675f1f
Mauricio62 <ericksandovalv08@gmail.com>
2025-06-26 23:33:57 -0500

    implementando userDetailService-SpringSecurity

--- src/main/java/pe/com/techpress/ProjectTechPress/Services/UserDetailsServiceImpl.java
+++ src/main/java/pe/com/techpress/ProjectTechPress/Services/UserDetailsServiceImpl.java
@@ -1,4 +1,26 @@
 package pe.com.techpress.ProjectTechPress.Services;
 
-public class UserDetailsServiceImpl {
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pe.com.techpress.ProjectTechPress.Models.SecurityUser;
+import pe.com.techpress.ProjectTechPress.Models.User;
+import pe.com.techpress.ProjectTechPress.Repositories.UserRepository;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new SecurityUser(user);
+    }
 }
